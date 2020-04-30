60749db2b458eabbdbaf465e3ddad82962aaed0f
Dr.Kidd <rkidd@sunymaritime.edu>
2020-04-30 14:24:04 -0400

    First commit

---
+++ MAVLink_connection.cpp
@@ -0,0 +1,232 @@
+//
+// Simple example to demonstrate how to use the MAVSDK.
+//
+// Author: Julian Oes <julian@oes.ch>
+
+#include <chrono>
+#include <cstdint>
+#include <mavsdk/mavsdk.h>
+#include <mavsdk/plugins/action/action.h>
+#include <mavsdk/plugins/telemetry/telemetry.h>
+#include <iostream>
+#include <thread>
+
+//UART info from https://raspberry-projects.com/pi/programming-in-c/uart-serial-port/using-the-uart
+#include <unistd.h>     //Used for UART
+#include <fcntl.h>      //Used for UART
+#include <termios.h>    //Used for UART
+
+using namespace mavsdk;
+using namespace std::this_thread;
+using namespace std::chrono;
+
+//Global Vars
+int uart0_filestream = -1;
+
+#define ERROR_CONSOLE_TEXT "\033[31m" // Turn text on console red
+#define TELEMETRY_CONSOLE_TEXT "\033[34m" // Turn text on console blue
+#define NORMAL_CONSOLE_TEXT "\033[0m" // Restore normal console colour
+
+void usage(std::string bin_name)
+{
+    std::cout << NORMAL_CONSOLE_TEXT << "Usage : " << bin_name << " <connection_url>" << std::endl
+              << "Connection URL format should be :" << std::endl
+              << " For TCP : tcp://[server_host][:server_port]" << std::endl
+              << " For UDP : udp://[bind_host][:bind_port]" << std::endl
+              << " For Serial : serial:///path/to/serial/dev[:baudrate]" << std::endl
+              << "For example, to connect to the simulator use URL: udp://:14540" << std::endl;
+}
+
+void component_discovered(ComponentType component_type)
+{
+    std::cout << NORMAL_CONSOLE_TEXT << "Discovered a component with type "
+              << unsigned(component_type) << std::endl;
+}
+
+void setup_uart()
+{
+    uart0_filestream = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY);
+    if(uart0_filestream == -1)
+    {
+        //ERROR - CAN'T OPEN SERIAL PORT
+        std::cout <<"ERROR - Unable to open UART (Serial for Arduino)" << std::endl;
+    }
+    struct termios options;
+    tcgetattr(uart0_filestream, &options);
+    options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;    //CHECK BAUD RATE!
+    options.c_iflag = IGNPAR;
+    options.c_oflag = 0;
+    options.c_lflag = 0;
+    tcflush(uart0_filestream, TCIFLUSH);
+    tcsetattr(uart0_filestream, TCSANOW, &options);
+}
+
+int send_to_serial(std::string toSerial)
+{
+    //Insert code here
+    //If the Arduino comes up after this program, restart port
+/*    //----- CHECK FOR ANY RX BYTES -----	
+    if (uart0_filestream != -1)
+	{
+		// Read up to 255 characters from the port if they are there
+		unsigned char rx_buffer[256];
+		int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);		//Filestream, buffer to store in, number of bytes to read (max)
+		if (rx_length < 0)
+		{
+			//An error occured (will occur if there are no bytes)
+		}
+		else if (rx_length == 0)
+		{
+			//No data waiting
+		}
+		else
+		{
+			//Bytes received
+			rx_buffer[rx_length] = '\0';
+			printf("%i bytes read : %s\n", rx_length, rx_buffer);
+            int rlen = write(uart0_filestream, toSerial.c_str(), toSerial.size());
+            std::cout << toSerial << std::endl;
+            return rlen;
+		}
+	}    
+    return 0;
+*/
+            int rlen = write(uart0_filestream, toSerial.c_str(), toSerial.size());
+            std::cout << toSerial << std::endl;
+            return rlen;
+
+} 
+
+int main(int argc, char** argv)
+{
+    Mavsdk dc;
+    std::string connection_url;
+    ConnectionResult connection_result;
+    setup_uart();
+
+    bool discovered_system = false;
+    if (argc == 2) {
+        connection_url = argv[1];
+        connection_result = dc.add_any_connection(connection_url);
+    } else {
+        usage(argv[0]);
+        return 1;
+    }
+
+    if (connection_result != ConnectionResult::SUCCESS) {
+        std::cout << ERROR_CONSOLE_TEXT
+                  << "Connection failed: " << connection_result_str(connection_result)
+                  << NORMAL_CONSOLE_TEXT << std::endl;
+        return 1;
+    }
+
+    // We don't need to specify the UUID if it's only one system anyway.
+    // If there were multiple, we could specify it with:
+    // dc.system(uint64_t uuid);
+    System& system = dc.system();
+
+    std::cout << "Waiting to discover system..." << std::endl;
+    dc.register_on_discover([&discovered_system](uint64_t uuid) {
+        std::cout << "Discovered system with UUID: " << uuid << std::endl;
+        discovered_system = true;
+    });
+
+    // We usually receive heartbeats at 1Hz, therefore we should find a system after around 2
+    // seconds.
+    sleep_for(seconds(2));
+
+    if (!discovered_system) {
+        std::cout << ERROR_CONSOLE_TEXT << "No system found, exiting." << NORMAL_CONSOLE_TEXT
+                  << std::endl;
+        return 1;
+    }
+
+    // Register a callback so we get told when components (camera, gimbal) etc
+    // are found.
+    system.register_component_discovered_callback(component_discovered);
+
+    auto telemetry = std::make_shared<Telemetry>(system);
+    auto action = std::make_shared<Action>(system);
+
+    // We want to listen to the altitude of the drone at 1 Hz.
+    const Telemetry::Result set_rate_result = telemetry->set_rate_position(1.0);
+    if (set_rate_result != Telemetry::Result::SUCCESS) {
+        std::cout << ERROR_CONSOLE_TEXT
+                  << "Setting rate failed:" << Telemetry::result_str(set_rate_result)
+                  << NORMAL_CONSOLE_TEXT << std::endl;
+        return 1;
+    }
+
+    // Set up callback to monitor altitude while the vehicle is in flight
+    telemetry->position_async([](Telemetry::Position position) {
+        std::cout << TELEMETRY_CONSOLE_TEXT // set to blue
+                  << "Lat: " << position.latitude_deg << " deg\n"
+                  << "Lon: " << position.longitude_deg << " deg"
+                  << NORMAL_CONSOLE_TEXT // set to default color again
+                  << std::endl;
+        //TESTING STUFF!!!
+        if(position.latitude_deg == 0 && position.longitude_deg ==0)
+        {
+            position.latitude_deg = 26.2;
+            position.longitude_deg = -54.33;
+        }
+        //END TESTING!!!!
+        std::string tempString = "Lat,";
+        tempString = tempString + std::to_string(position.latitude_deg) + ", Lon," + std::to_string(position.longitude_deg) + "\n";         
+        send_to_serial(tempString);
+    });
+
+    while(true)
+    {
+        sleep_for(milliseconds(250));
+    }
+
+/*    // Check if vehicle is ready to arm
+    while (telemetry->health_all_ok() != true) {
+        std::cout << "Vehicle is getting ready to arm" << std::endl;
+        sleep_for(seconds(1));
+    }
+
+    // Arm vehicle
+    std::cout << "Arming..." << std::endl;
+    const Action::Result arm_result = action->arm();
+
+    if (arm_result != Action::Result::SUCCESS) {
+        std::cout << ERROR_CONSOLE_TEXT << "Arming failed:" << Action::result_str(arm_result)
+                  << NORMAL_CONSOLE_TEXT << std::endl;
+        return 1;
+    }
+
+    // Take off
+    std::cout << "Taking off..." << std::endl;
+    const Action::Result takeoff_result = action->takeoff();
+    if (takeoff_result != Action::Result::SUCCESS) {
+        std::cout << ERROR_CONSOLE_TEXT << "Takeoff failed:" << Action::result_str(takeoff_result)
+                  << NORMAL_CONSOLE_TEXT << std::endl;
+        return 1;
+    }
+
+    // Let it hover for a bit before landing again.
+    sleep_for(seconds(10));
+
+    std::cout << "Landing..." << std::endl;
+    const Action::Result land_result = action->land();
+    if (land_result != Action::Result::SUCCESS) {
+        std::cout << ERROR_CONSOLE_TEXT << "Land failed:" << Action::result_str(land_result)
+                  << NORMAL_CONSOLE_TEXT << std::endl;
+        return 1;
+    }
+
+    // Check if vehicle is still in air
+    while (telemetry->in_air()) {
+        std::cout << "Vehicle is landing..." << std::endl;
+        sleep_for(seconds(1));
+    }
+    std::cout << "Landed!" << std::endl;
+
+    // We are relying on auto-disarming but let's keep watching the telemetry for a bit longer.
+    sleep_for(seconds(3));
+    std::cout << "Finished..." << std::endl;
+*/
+    return 0;
+}
---
+++ build/CMakeFiles/MAVLink_connection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MAVLink_connection.dir/MAVLink_connection.cpp.o"
+  "MAVLink_connection.pdb"
+  "MAVLink_connection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MAVLink_connection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
